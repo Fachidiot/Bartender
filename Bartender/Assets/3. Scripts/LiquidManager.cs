2734004ac0dd418b3bf643c33497c110bb83f08f
Fachidiot <54926396+Fachidiot@users.noreply.github.com>
2025-06-05 10:44:59 +0900

    # Data setting

--- Bartender/Assets/3. Scripts/LiquidManager.cs
+++ Bartender/Assets/3. Scripts/LiquidManager.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class LiquidManager : MonoBehaviour
 {
-    [SerializeField]
-    private LiquidData liquidData;
-    [SerializeField]
-    [Range(0f, 1f)]
-    private float remainValue = 1f;
-    [SerializeField]
-    [Range(-18f, 27f)]
-    private float liquidTemperature = -18f;
+    [SerializeField] private LiquidData liquidData;
+    [SerializeField] private GameObject liquidObject;
+    [SerializeField][Range(0f, 1f)] private float remainValue = 1f;
+    [SerializeField][Range(-18f, 27f)] private float liquidTemperature = -18f;
 
     void Start()
     {
@@ -21,6 +17,6 @@ public class LiquidManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
